ec68ae4a33bf09d2dc32d75a4ca90f287009074a
Vicky Chijwani <vickychijwani@gmail.com>
2016-06-09 02:30:45 +0530

    Add free/paid flavors, remove unrequired dependencies from paid flavor.

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,18 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends MainActivityFragmentBase {
+
+    public MainActivityFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
