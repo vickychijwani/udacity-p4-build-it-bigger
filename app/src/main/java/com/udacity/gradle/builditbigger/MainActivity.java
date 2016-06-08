e6298a9f66a3e70d282ae9a777e15f26e3a374e4
Vicky Chijwani <vickychijwani@gmail.com>
2016-06-08 22:32:14 +0530

    Add Java library that returns random canned jokes.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import me.vickychijwani.udacity.libjoke.JokeFactory;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -39,8 +41,9 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+    public void tellJoke(View view) {
+        String randomJoke = JokeFactory.getRandomJoke();
+        Toast.makeText(this, randomJoke, Toast.LENGTH_SHORT).show();
     }
 
 
